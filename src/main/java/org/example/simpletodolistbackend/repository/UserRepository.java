78752a89ae46f1e76972c713dc21c1223f76ae5a
socilt54 <socilt54@gmail.com>
2024-09-22 19:30:15 +0900

    feat: Add findByEmail method to UserRepository

--- src/main/java/org/example/simpletodolistbackend/repository/UserRepository.java
+++ src/main/java/org/example/simpletodolistbackend/repository/UserRepository.java
@@ -3,5 +3,8 @@ package org.example.simpletodolistbackend.repository;
 import org.example.simpletodolistbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
