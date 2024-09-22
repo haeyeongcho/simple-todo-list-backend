f2e8b0c6e2402fcff5977b3645e1982975e6be15
socilt54 <socilt54@gmail.com>
2024-09-22 18:23:35 +0900

    init: Project initialization

---
+++ src/main/java/org/example/simpletodolistbackend/SimpleTodoListBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.simpletodolistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoListBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTodoListBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/simpletodolistbackend/SimpleTodoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simpletodolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
