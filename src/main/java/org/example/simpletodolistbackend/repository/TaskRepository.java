42f270910a48eb845a53434e8a67ead432532ffe
socilt54 <socilt54@gmail.com>
2024-09-24 16:43:21 +0900

    feat: 사용자별 작업 조회를 위한 findByUser 메서드 추가

--- src/main/java/org/example/simpletodolistbackend/repository/TaskRepository.java
+++ src/main/java/org/example/simpletodolistbackend/repository/TaskRepository.java
@@ -1,7 +1,11 @@
 package org.example.simpletodolistbackend.repository;
 
 import org.example.simpletodolistbackend.entity.Task;
+import org.example.simpletodolistbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUser(User user);
 }
